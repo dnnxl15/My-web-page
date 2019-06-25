f2a1649965d3aa10e113e6618c53d6aa28263c79
Danny <dnnxl15@gmail.com>
2019-06-25 06:13:12 -0600

    Update some routes

--- server/controllers/project.js
+++ server/controllers/project.js
@@ -1,4 +1,3 @@
-'use strict'
 
 var controller =
 {
@@ -18,24 +17,83 @@ var controller =
 
     saveProject: function(req, res)
     {
-        var project = new Project();
-
+        var project = new project();
         var params = req.body;
         project.name = params.name;
-        project.year = params.year;
+        project.description = params.description;
         project.category = params.category;
+        project.year = params.year;
         project.langs = params.langs;
         project.image = null;
 
-        project.save((err, projectStored) => {
-            if(err) return res.status(500).send({message: "Error to save the project"});
+        project.save((err, projectStored) =>
+        {
+            if(err) return res.status(500).send({message: "Error to save the document"});
 
-            if(!projectStored) return res.status(404).send({message: "Can't save the project"});
+            if(!projectStored) return res.status(404).send({message: 'Couldnt save the file'});
 
             return res.status(200).send({project: projectStored});
-
         });
-        
-    }
+    },
 
-}
+    getProject: function(req, res)
+    {
+        var projectId = req.params.id;
+
+        if(projectId == null) return res.status(404).send({message: 'The project doesnt exists'});
+
+        Project.findById(projectId, (err, project) => {
+
+            if(err) return res.status(500).send({message: "Error to get the document"});
+
+            if(!projectStored) return res.status(404).send({message: 'Couldnt find the file'});
+
+            return res.status(200).send({project});
+        });
+    },
+
+    getProjects: function(req, res)
+    {
+        var projectId = req.params.id;
+
+        if(projectId == null) return res.status(404).send({message: 'The project doesnt exists'});
+
+        Project.find({}).sort('year').exe((err, project) => {
+
+            if(err) return res.status(500).send({message: "Error to get the documents"});
+
+            if(!projectStored) return res.status(404).send({message: 'Couldnt find the file'});
+
+            return res.status(200).send({project});
+        });
+    },
+
+    updateProject: function(req, res)
+    {
+        var projectId = req.params.id;
+        var update = req.body;
+
+        Project.findByIdAndUpdate(projectId, update, (err, projectUpdate) => {
+            if(err) return res.status(500).send({message: "Error to update the documents"});
+
+            if(!projectUpdate) return res.status(404).send({message: 'Couldnt update the file'});
+
+            return res.status(200).send({project: projectUpdate});
+        });
+    },
+
+    deleteProject: function(req, res)
+    {
+        var projectId = req.params.id;
+        Project.findByIdAndDELETE(projectId, (err, projectRemoved) =>
+        {
+            if(err) return res.status(500).send({message: "Error to delete the documents"});
+
+            if(!projectUpdate) return res.status(404).send({message: 'Couldnt delete the file'});
+
+            return res.status(200).send({project: projectRemoved});
+        });
+    }
+};
+
+module.exports = controller;
--- server/routes/project.js
+++ server/routes/project.js
@@ -1,4 +1,3 @@
-'use strict'
 
 var express = require('express');
 var ProjectrController = require('../controllers/project');
@@ -6,6 +5,10 @@ var ProjectrController = require('../controllers/project');
 var router = express.Router();
 
 router.get('/home', ProjectrController.home);
-router.get('/test', ProjectrController.test);
+router.post('/test', ProjectrController.test);
+router.post('/save-project', ProjectrController.saveProject);
+router.get('/project/:id?', ProjectrController.getProject);
+router.get('/project', ProjectrController.getProjects);
+router.put('/project/:id', ProjectrController.updateProject);
 
 module.exports = router;
