c03cd23c62b3cb71322915cfbc3faae8a9e21c4a
Danny <dnnxl15@gmail.com>
2019-06-24 21:35:32 -0600

    Save project function

--- server/controllers/project.js
+++ server/controllers/project.js
@@ -14,7 +14,28 @@ var controller =
         return res.status(200).send({
             message: 'I am method or action test of the controller'
         });
-    }
+    },
 
+    saveProject: function(req, res)
+    {
+        var project = new Project();
+
+        var params = req.body;
+        project.name = params.name;
+        project.year = params.year;
+        project.category = params.category;
+        project.langs = params.langs;
+        project.image = null;
+
+        project.save((err, projectStored) => {
+            if(err) return res.status(500).send({message: "Error to save the project"});
+
+            if(!projectStored) return res.status(404).send({message: "Can't save the project"});
+
+            return res.status(200).send({project: projectStored});
+
+        });
+        
+    }
 
 }
